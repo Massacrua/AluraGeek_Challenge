32bc78d95db953313ffd3d3c034c7fe6713a6b95
Guilherme Dalla Rosa <gui.dallarosa@gmail.com>
2023-08-02 17:34:28 -0300

    Função adicionar produto

--- conectaApi.js
+++ conectaApi.js
@@ -1,12 +1,35 @@
 const endpoint = "https://64c9ab01b2980cec85c257c2.mockapi.io/products"
 
 async function getProducts() {
-    const r = await fetch(endpoint)
-    const produtos = await r.json()
+    const conexao = await fetch(endpoint)
+    const produtos = await conexao.json()
     console.log(produtos)
     return produtos
 }
 
+async function criaNovoProduto(nome, preco, categoria, descricao, imageurl) {
+    const conexao = await fetch(endpoint, {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            nome: nome,
+            categoria: categoria,
+            preco: preco,
+            descricao: descricao,
+            imageurl: imageurl
+        })
+    })
+    if (!conexao.ok) {
+        throw new Error("Não foi possível criar o produto")
+    }
+
+    const conexaoConvertida = await conexao.json()
+    return conexaoConvertida
+}
+
 export const conectaAPI = {
-    getProducts
+    getProducts,
+    criaNovoProduto
 }
--- formAddProduto.js
+++ formAddProduto.js
@@ -1,8 +1,24 @@
-(() => {
+import { conectaAPI } from "./conectaApi.js"
 const addProductForm = document.querySelector(".addproduto__form")
 addProductForm.addEventListener("submit", event => {
     event.preventDefault()
-    window.location.href = "./produtos-home.html"
+    criarProduto()
 })
 
-})()
+async function criarProduto() {
+    const imageurl = document.querySelector("#img-url").value
+    const categoria = document.querySelector("#categoria").value
+    const nome = document.querySelector("#produto-nome").value
+    const preco = document.querySelector("#produto-preco").value
+    const descricao = document.querySelector("#produto-descricao").value
+
+    try {
+        await conectaAPI.criaNovoProduto(nome, preco, categoria, descricao, imageurl)
+        window.location.href = "./produtos-home.html"
+    } catch (e) {
+        alert(e)
+    }
+
+}
+
+
